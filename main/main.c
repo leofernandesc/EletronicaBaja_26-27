199939d21a46e4556d5e820c161e35eedbf301b3
pedroituassu <185136670+pedroituassu@users.noreply.github.com>
2026-07-22 11:52:11 -0400

    feat: add mock values to datalogger

--- main/main.c
+++ main/main.c
@@ -1,6 +1,7 @@
 #include "datalogger.h"
 #include "driver/sdspi_host.h"
 #include "esp_err.h"
+#include "esp_timer.h"
 #include "sdmmc_cmd.h"
 
 #define MAX_CHAR_SIZE 64
@@ -17,9 +18,18 @@ void app_main(void) {
   sdmmc_card_print_info(stdout, card);
 
   const char *car_data = MOUNT_POINT "/car_data.csv";
+
   char data[MAX_CHAR_SIZE];
   snprintf(data, MAX_CHAR_SIZE,
            "timestamp,rpm,speed1,speed2,pressure1,pressure2\n");
+  ret = datalogger_append_to_file(car_data, data);
+  if (ret != ESP_OK) {
+    return;
+  }
+
+  snprintf(data, MAX_CHAR_SIZE, "%lld,%d,%.2f,%.2f,%.2f,%.2f\n",
+           esp_timer_get_time() / 1000000, 2500, 20.00, 20.00, 1000.00,
+           1000.00);
   ret = datalogger_append_to_file(car_data, data);
   if (ret != ESP_OK) {
     return;
